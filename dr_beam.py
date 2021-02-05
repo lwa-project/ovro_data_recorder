ead85826237f2777b658802151f537c191654b04
jaycedowell <jayce.dowell@gmail.com>
2021-02-04 17:41:13 -0700

    Added a stub pipeline.

---
+++ dr_beam.py
@@ -0,0 +1,75 @@
+#!/usr/env python
+
+import os
+import sys
+import h5py
+import time
+import numpy
+import logging
+import argparse
+
+from common import *
+
+
+class CaptureOp(object):
+    pass
+
+
+class ProcessingOp(object):
+    pass
+
+
+class WriterOp(object):
+    pass
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+                 description="Data recorder for power beams"
+                 )
+    parser.add_argument('-a', '--address', type=str, default='127.0.0.1',
+                        help='IP address to listen to')
+    parser.add_argument('-p', '--port', type=int, default=10000,
+                        help='UDP port to receive data on')
+    parser.add_argument('-o', '--offline', type=str,
+                        help='run in offline using the specified file to read from')
+    parser.add_argument('-b', '--beam', type=int, default=1,
+                        help='beam to receive data for')
+    parser.add_argument('-g', '--gulp-size', type=int, default=1024,
+                        help='gulp size for ring buffers')
+    parser.add_argument('-f', '--fork', action='store_true',
+                        help='fork and run in the background')
+    args = parser.parse_args()
+    
+    # Fork, if requested
+    if args.fork:
+        stderr = '/tmp/%s_%i.stderr' % (os.path.splitext(os.path.basename(__file__))[0], tuning)
+        daemonize(stdin='/dev/null', stdout='/dev/null', stderr=stderr)
+        
+    # Setup logging
+    log = logging.getLogger(__name__)
+    logFormat = logging.Formatter('%(asctime)s [%(levelname)-8s] %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    logFormat.converter = time.gmtime
+    logHandler = logging.StreamHandler(sys.stdout)
+    
+    # Setup the blocks
+    ops = []
+    
+    # Launch!
+    log.info("Launching %i thread(s)", len(threads))
+    for thread in threads:
+        #thread.daemon = True
+        thread.start()
+    while not shutdown_event.is_set():
+        signal.pause()
+    log.info("Shutdown, waiting for threads to join")
+    for thread in threads:
+        thread.join()
+    log.info("All done")
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+    
