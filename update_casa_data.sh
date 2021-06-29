b96860da3a20b6ea258faa6ff3c47c7fe2f12dc2
jaycedowell <jayce.dowell@gmail.com>
2021-06-29 16:09:15 +0000

    OVRO-LWA check.

--- update_casa_data.sh
+++ update_casa_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mkdir -p /home/ubuntu/anaconda3/envs/casa/lib/casa/data/
-cd /home/ubuntu/anaconda3/envs/casa/lib/casa/data/
-rsync -avz rsync://casa-rsync.nrao.edu/casa-data .
+if [[ -e /home/ubuntu/anaconda3/envs/casa ]]; then
+  mkdir -p /home/ubuntu/anaconda3/envs/casa/lib/casa/data/
+  cd /home/ubuntu/anaconda3/envs/casa/lib/casa/data/
+  rsync -avz rsync://casa-rsync.nrao.edu/casa-data .
+fi
