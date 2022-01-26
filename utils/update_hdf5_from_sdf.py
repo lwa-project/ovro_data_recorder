f4f6bde91e683745b759c72d51acef5e6715744f
jaycedowell <jayce.dowell@gmail.com>
2022-01-26 07:51:25 -0700

    Use argparse.

--- utils/update_hdf5_from_sdf.py
+++ utils/update_hdf5_from_sdf.py
@@ -3,11 +3,23 @@
 import os
 import sys
 import h5py
+import argparse
 
 from data import fill_from_sdf
 
-h = h5py.File(sys.argv[1], mode='a')
-s = sys.argv[2]
+def main(args):
+    print(f"Updating {os.path.basename(args.filename)} using {os.path.basename(args.sdfname)}")
+    h = h5py.File(args.filename, mode='a')
+    fill_from_sdf(h, args.sdfname, station='ovrolwa')
+    h.close()
 
-fill_from_sdf(h, s, station='ovrolwa')
-h.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                description='Update the observation metadata in a HDF5 using the SDF that caused it to be recorded')
+    parser.add_argument('filename', type=str,
+                        help='HDF5 to update')
+    parser.add_argument('sdfname', type=str,
+                        help='associated SDF to use')
+    args = parser.parse_args()
+    main(args)
