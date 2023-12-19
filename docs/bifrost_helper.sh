0423a324da5b82626844327649be4ef6847766be
jaycedowell <jayce.dowell@gmail.com>
2023-12-18 17:47:02 -0700

    Getting closer.

--- docs/bifrost_helper.sh
+++ docs/bifrost_helper.sh
@@ -2,6 +2,7 @@
 
 ROOT_PATH=`dirname $0`
 cd ${ROOT_PATH}/../bifrost
-./configure
+INSTALL_PREFIX=`python -c "import os,sys; print(os.path.dirname(os.path.dirname(sys.executable)))"`
+./configure --prefix=${INSTALL_PREFIX}
 make -j all
 make install
