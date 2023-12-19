a17a579a1aa1fcfab37f9c21354f4ffea0cfb401
jaycedowell <jayce.dowell@gmail.com>
2023-12-19 07:27:49 -0700

    Getting closer?

--- docs/.rtd_rsyslog_helper.sh
+++ docs/.rtd_rsyslog_helper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo bash -ec "echo -e \"module(load='imudp')\ninput(type='imudp' port='514')\"" >> /etc/rsyslog.conf
+sudo bash -ec "echo -e \"module(load='imudp')\ninput(type='imudp' port='514')\" >> /etc/rsyslog.conf"
 sudo restart rsyslog
